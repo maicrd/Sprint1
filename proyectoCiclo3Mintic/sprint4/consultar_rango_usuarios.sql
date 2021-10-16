4ba8eca16cd2eef7f74d9260ae47abf9c626a383
JimmyDevPasto <80181029+JimmyDevPasto@users.noreply.github.com>
2021-10-15 21:44:11 -0500

    "sprint 4"
    "sprint 4"

---
+++ proyectoCiclo3Mintic/sprint4/consultar_rango_usuarios.sql
@@ -0,0 +1,3 @@
+select usuario, contrasena from usuarios WHERE id Between 6 and  10;
+
+
---
+++ proyectoCiclo3Mintic/sprint4/consultar_top_usuarios.sql
@@ -0,0 +1,6 @@
+
+
+
+SELECT id, usuario, contrasena, fecha_registro FROM usuarios 
+ ORDER BY id DESC
+ LIMIT 5;
